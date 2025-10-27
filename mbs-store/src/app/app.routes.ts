66003555d541bfe9ca6781baeb2cb5d17764b596
YandryJBM <121211394+YandryJBM@users.noreply.github.com>
2025-10-27 14:30:39 -0500

    inicio

--- mbs-store/src/app/app.routes.ts
+++ mbs-store/src/app/app.routes.ts
@@ -5,9 +5,15 @@ import { InicioComponent } from './features/inicio/inicio.component';
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    component: LayoutComponent, // contiene header y footer
     children: [
-      { path: '', component: InicioComponent }
+      {
+        path: '',
+        component: InicioComponent // se carga dentro del <router-outlet> del layout
+      },
+      // puedes agregar más secciones aquí:
+      // { path: 'productos', component: ProductosComponent },
+      // { path: 'contacto', component: ContactoComponent },
     ]
   }
 ];
